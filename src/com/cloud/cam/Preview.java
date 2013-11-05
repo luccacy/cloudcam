6f1c412a2b357bdfc35f4b4eb69d831c4cba1009
luccacy <luccacy@yahoo.com>
2013-11-05 22:28:57 +0800

    add source

--- src/com/cloud/cam/Preview.java
+++ src/com/cloud/cam/Preview.java
@@ -6,9 +6,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ImageFormat;
@@ -24,13 +26,15 @@ import android.media.CamcorderProfile;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class Preview extends SurfaceView implements SurfaceHolder.Callback {
+public class Preview extends SurfaceView implements SurfaceHolder.Callback,SensorEventListener {
 	private static String TAG = "Preview";
 	private SurfaceHolder mHolder;
 	private Camera mCamera;
@@ -39,6 +43,10 @@ public class Preview extends SurfaceView implements SurfaceHolder.Callback {
 	private boolean isRecording = false;
 	private Paint paint = new Paint();
 
+	private float a_x;
+	private float a_y;
+	private float a_z;
+		
 	public Preview(Context context, Bundle savedInstanceState) {
 		super(context);
 		mCamera = Camera.open();
@@ -60,6 +68,10 @@ public class Preview extends SurfaceView implements SurfaceHolder.Callback {
 					mCamera = null;
 				}
 			}
+			
+			Activity activity = (Activity)this.getContext();
+			ImageButton view = (ImageButton)activity.findViewById(R.id.take_photo);
+			view.setImageResource(isRecording ? R.drawable.off : R.drawable.on);
 
 			if (mMediaRecorder == null)
 				mMediaRecorder = new MediaRecorder();
@@ -90,6 +102,10 @@ public class Preview extends SurfaceView implements SurfaceHolder.Callback {
 			}
 			isRecording = true;
 		} else {
+			Activity activity = (Activity)this.getContext();
+			ImageButton view = (ImageButton)activity.findViewById(R.id.take_photo);
+			view.setImageResource(isRecording ? R.drawable.off : R.drawable.on);
+			
 			mMediaRecorder.stop();
 			releaseMediaRecorder();
 			isRecording = false;
@@ -97,43 +113,11 @@ public class Preview extends SurfaceView implements SurfaceHolder.Callback {
 			try {
 				mCamera = Camera.open();
 				mCamera.setPreviewDisplay(mHolder);
-				mCamera.setPreviewCallback(new StreamIt(isRecording));
+//				mCamera.setPreviewCallback(new StreamIt(isRecording));
 				mCamera.startPreview();
 				isPreview = true;
 			} catch (Exception e) {
 				e.printStackTrace();
-			}
-		}
-	}
-
-	public void onPreviewFrame(byte[] data, Camera camera) {
-		Log.e(TAG, "=========on Preview frame");
-		// 刚刚拍照的文件名
-		if (true) {
-			String fileName = "IMG_"
-					+ new SimpleDateFormat("yyyyMMdd_HHmmss")
-							.format(new Date()).toString() + ".jpg";
-			File sdRoot = Environment.getExternalStorageDirectory();
-			String dir = "/DCIM/";
-			File mkDir = new File(sdRoot, dir);
-			if (!mkDir.exists())
-				mkDir.mkdirs();
-			File pictureFile = new File(sdRoot, dir + fileName);
-			if (!pictureFile.exists()) {
-				try {
-					pictureFile.createNewFile();
-					Camera.Parameters parameters = camera.getParameters();
-					Size size = parameters.getPreviewSize();
-					YuvImage image = new YuvImage(data,
-							parameters.getPreviewFormat(), size.width,
-							size.height, null);
-					FileOutputStream filecon = new FileOutputStream(pictureFile);
-					image.compressToJpeg(
-							new Rect(0, 0, image.getWidth(), image.getHeight()),
-							90, filecon);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
 			}
 		}
 	}
@@ -154,7 +138,8 @@ public class Preview extends SurfaceView implements SurfaceHolder.Callback {
 		final float scale = getResources().getDisplayMetrics().density;
 		paint.setTextSize(14 * scale + 0.5f); // convert dps to pixels
 		paint.setTextAlign(Paint.Align.CENTER);
-		canvas.drawText("hello world", canvas.getWidth() / 2,
+		
+		canvas.drawText("x:" + a_x + "y:" + a_y + "z:" +a_z, canvas.getWidth() / 2,
 				canvas.getHeight() / 3, paint);
 
 		canvas.restore();
@@ -164,9 +149,16 @@ public class Preview extends SurfaceView implements SurfaceHolder.Callback {
 		// The Surface has been created, now tell the camera where to draw the
 		// preview.
 		try {
-			mCamera.setPreviewDisplay(holder);
+			Log.e(TAG, "=====1======");
+			if(holder == null){
+				Log.e(TAG, "===null holder===");
+			}
+			
+			mCamera.setPreviewDisplay(mHolder);
+			Log.e(TAG, "=====2======");
 			mCamera.startPreview();
-			mCamera.setPreviewCallback(new StreamIt(isRecording));
+			Log.e(TAG, "=====3======");
+			//mCamera.setPreviewCallback(new StreamIt(isRecording));
 			isPreview = true;
 			this.setWillNotDraw(false);
 		} catch (IOException e) {
@@ -176,6 +168,9 @@ public class Preview extends SurfaceView implements SurfaceHolder.Callback {
 
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		// empty. Take care of releasing the Camera preview in your activity.
+		Log.e(TAG, "==========surface destroy");
+		Log.e(TAG, "==========surface destroy");
+		Log.e(TAG, "==========surface destroy");
 		if (mCamera != null) {
 			if (isPreview) {
 				mCamera.stopPreview();
@@ -184,7 +179,7 @@ public class Preview extends SurfaceView implements SurfaceHolder.Callback {
 			mCamera.release();
 			mCamera = null; // 记得释放
 		}
-
+		
 		mHolder = null;
 	}
 
@@ -199,26 +194,41 @@ public class Preview extends SurfaceView implements SurfaceHolder.Callback {
 
 		// stop preview before making changes
 		try {
-			// mCamera.stopPreview();
+			 mCamera.stopPreview();
 		} catch (Exception e) {
 			// ignore: tried to stop a non-existent preview
 		}
-
+		setPreviewSize();
 		// set preview size and make any resize, rotate or
 		// reformatting changes here
 
 		// start preview with new settings
 		try {
-			// mCamera.setPreviewDisplay(mHolder);
-			// mCamera.startPreview();
-			// isPreview = true;
+			 mCamera.setPreviewDisplay(mHolder);
+			 mCamera.startPreview();
+			 isPreview = true;
 
 		} catch (Exception e) {
 			Log.d(TAG, "Error starting camera preview: " + e.getMessage());
 		}
 	}
 
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// TODO Auto-generated method stub
+		a_x = event.values[0];
+		a_y = event.values[1];
+		a_z = event.values[2];
+		
+		this.invalidate();
+		//this.showToast(null, "x:" + x + "y:" + y + "z:" + z);
+	}
 
 	public Toast showToast(Toast clear_toast, String message) {
 		class RotatedTextView extends View {
@@ -281,7 +291,7 @@ public class Preview extends SurfaceView implements SurfaceHolder.Callback {
 		return clear_toast;
 	}
 
-	private void setFocus(String focus_value) {
+	public void setFocus(String focus_value) {
 		if (MyDebug.LOG)
 			Log.d(TAG, "setFocus() " + focus_value);
 		Camera.Parameters parameters = mCamera.getParameters();
@@ -336,6 +346,74 @@ public class Preview extends SurfaceView implements SurfaceHolder.Callback {
 			}
 		}
 	}
+	
+	private void setPreviewSize() {
+		if( MyDebug.LOG )
+			Log.d(TAG, "setPreviewSize()");
+		if( mCamera == null ) {
+			return;
+		}
+		// set optimal preview size
+    	Camera.Parameters parameters = mCamera.getParameters();
+		if( MyDebug.LOG )
+			Log.d(TAG, "current preview size: " + parameters.getPreviewSize().width + ", " + parameters.getPreviewSize().height);
+    	Camera.Size current_size = parameters.getPictureSize();
+		if( MyDebug.LOG )
+			Log.d(TAG, "current size: " + current_size.width + ", " + current_size.height);
+        List<Camera.Size> preview_sizes = parameters.getSupportedPreviewSizes();
+        if( preview_sizes.size() > 0 ) {
+	        Camera.Size best_size = preview_sizes.get(0);
+	        for(Camera.Size size : preview_sizes) {
+	    		if( MyDebug.LOG )
+	    			Log.d(TAG, "    supported preview size: " + size.width + ", " + size.height);
+	        	if( size.width*size.height > best_size.width*best_size.height ) {
+	        		best_size = size;
+	        	}
+	        }
+            parameters.setPreviewSize(best_size.width, best_size.height);
+    		if( MyDebug.LOG )
+    			Log.d(TAG, "new preview size: " + parameters.getPreviewSize().width + ", " + parameters.getPreviewSize().height);
+
+    		/*List<int []> fps_ranges = parameters.getSupportedPreviewFpsRange();
+    		if( MyDebug.LOG ) {
+		        for(int [] fps_range : fps_ranges) {
+	    			Log.d(TAG, "    supported fps range: " + fps_range[Camera.Parameters.PREVIEW_FPS_MIN_INDEX] + " to " + fps_range[Camera.Parameters.PREVIEW_FPS_MAX_INDEX]);
+		        }
+    		}
+    		int [] fps_range = fps_ranges.get(fps_ranges.size()-1);
+	        parameters.setPreviewFpsRange(fps_range[Camera.Parameters.PREVIEW_FPS_MIN_INDEX], fps_range[Camera.Parameters.PREVIEW_FPS_MAX_INDEX]);*/
+    		mCamera.setParameters(parameters);
+        }
+	}
+	
+    public void onResume() {
+		if( MyDebug.LOG )
+			Log.d(TAG, "onResume");
+
+		try {
+			mCamera = Camera.open();
+			mCamera.setPreviewDisplay(mHolder);
+//			mCamera.setPreviewCallback(new StreamIt(isRecording));
+//			mCamera.startPreview();
+			isPreview = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+    public void onPause() {
+		if( MyDebug.LOG )
+			Log.d(TAG, "onPause");
+
+		if (mCamera != null) {
+			if (isPreview) {
+				mCamera.stopPreview();
+				isPreview = false;
+			}
+			mCamera.release();
+			mCamera = null; // 记得释放
+		}
+    }
 }
 
 class StreamIt implements Camera.PreviewCallback {
@@ -347,10 +425,10 @@ class StreamIt implements Camera.PreviewCallback {
 	
 	
 	public void onPreviewFrame(byte[] data, Camera camera) {
-		Log.e(TAG, "=========on Preview frame");
+
 		// 刚刚拍照的文件名
 		if (true) {
-			Log.e(TAG, "=========on Preview frame func");
+
 			String fileName = "IMG_"
 					+ new SimpleDateFormat("yyyyMMdd_HHmmss")
 							.format(new Date()).toString() + ".jpg";
